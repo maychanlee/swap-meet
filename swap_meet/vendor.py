0295536feab3be91ecaf35ae73ccc770bd542a03
divya42536 <divya42536@gmail.com>
2025-09-29 20:28:22 -0700

    Started working on Wave1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,22 @@
 class Vendor:
-    pass
+    """ Represents a vendor that manages an inventory of items.
+    Attributes:inventory (list) Default =[]
+
+    Methods: 
+    add(item): Adds an item to the inventory.
+               Returns the item that was added.
+    remove(item): Removes the matching item from the inventory.
+            Returns the item that was removed.
+            Returns False if the item is not found.
+    """
+
+    def __init__(self,item,inventory =[ ]):
+        self.item = item
+        self.inventory = inventory
+
+    def add_inventory(self):
+        
+
+
+    def remove_inventory(self):
+
